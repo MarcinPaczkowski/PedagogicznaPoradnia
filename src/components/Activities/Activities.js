8430ab567d441e4bf14671dc9c15898d141255b9
m.paczkowski <>
2018-09-20 16:48:22 +0200

    Finished activities

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import Activity from './Activity';
+import wavesImage from '../../assets/wavy_line_png_by_maddielovesselly-d6kg87o.png';
 
 const activities = () => {
+	const wavesStyles = {
+		backgroundImage: `url(${wavesImage})`
+	};
 	return (
 		<div className="activities">
+			<div className="activities-waves" style={wavesStyles} />
 			<div className="container">
-				<h2 className="activities-title">Rodzaje zajęć</h2>
+				<h2 className="activities-title">Zajęcia</h2>
 				<div className="columns">
 					<div className="column">
 						<Activity title="Korepetycje" desc="testowy opis" />
 					</div>
 					<div className="column">
-						<Activity />
+						<Activity title="" desc="testowy opis" color="#86abff" />
 					</div>
 					<div className="column">
-						<Activity />
+						<Activity title="Korepetycje" desc="testowy opis" />
+					</div>
+				</div>
+				<div className="columns">
+					<div className="column">
+						<Activity title="Korepetycje" desc="testowy opis" color="#86abff" />
+					</div>
+					<div className="column">
+						<Activity title="" desc="testowy opis" />
+					</div>
+					<div className="column">
+						<Activity title="Korepetycje" desc="testowy opis" color="#86abff" />
 					</div>
 				</div>
 			</div>
--- src/components/Activities/Activity.js
+++ src/components/Activities/Activity.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
 const activity = props => {
+	const styles = {
+		backgroundColor: props.color || '#ffaad2'
+	};
 	return (
-		<div className="activity">
+		<div className="activity" style={styles}>
 			<h3 className="activity-title">{props.title || 'test123'}</h3>
 			<p className="activity-desc">
 				{props.desc || 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem harum odit.'}
